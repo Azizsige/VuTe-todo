89d7053ff9fa76565786da24932256cabb4e6fc8
azizsige <kurniawan00azizsige@gmail.com>
2022-10-15 17:55:26 +0700

    update: finish the app but not in production :(

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,74 @@
 <template>
-  <h1 class="text-white xl:text-[96px] my-8 text-center text-[26px] xl:m-8">
-    Todo
-    <font-awesome-icon icon="fa-solid fa-arrow-turn-down-left" />
-  </h1>
+  <div id="app">
+    <h1 class="text-white xl:text-[96px] my-8 text-center text-[26px] xl:m-8">
+      Todo
+      <font-awesome-icon icon="fa-solid fa-arrow-turn-down-left" />
+    </h1>
 
-  <div class="wrapper flex flex-col items-center mt-[7rem] xl:mt-0">
-    <div class="container w-[80%] xl:w-[666px] h-[412.45]">
-      <div class="container-input">
-        <form>
-          <div class="flex">
-            <label
-              for="search-dropdown"
-              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
-              >Your Email</label
-            >
-            <div class="relative w-full">
-              <p
-                v-if="todo == ''"
-                class="text-red-700 text-[16px] xl:text-lg absolute z-10 top-[-3rem]"
+    <div class="wrapper flex flex-col items-center mt-[7rem] xl:mt-0">
+      <div class="container w-[80%] xl:w-[666px] h-[412.45]">
+        <div class="container-input">
+          <form>
+            <div class="flex">
+              <label
+                for="search-dropdown"
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+                >Your Email</label
               >
-                Input Wajib Diisi
-              </p>
-              <p v-else class="text-red-700 absolute z-10 top-[-2rem]"></p>
-              <input
-                id="search-dropdown"
-                v-model="todo"
-                class="block p-2 xl:p-4 pr-16 w-full z-20 text-[16px] xl:text-2xl text-white bg-bgSecondary drop-shadow-3xl rounded-r-xl border-none focus:ring-transparent"
-                placeholder="Create a new todo..."
-                required
-                autocomplete="off"
-              />
-              <button
-                type="submit"
-                @click.prevent="addTodo"
-                class="absolute text-center w-10 h-10 xl:w-auto xl:h-auto top-0 right-0 xl:p-2.5 text-2xl font-medium text-white bg-addBtn rounded-r-lg border hover:bg-addBtn border-none"
-              >
-                <svg
-                  aria-hidden="true"
-                  class="h-6 w-6 m-0 mx-auto xl:w-11 xl:h-11"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div class="relative w-full">
+                <p
+                  v-if="todo == ''"
+                  class="text-red-700 text-[16px] xl:text-lg absolute z-10 top-[-3rem]"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  ></path>
-                </svg>
-              </button>
+                  Input Wajib Diisi
+                </p>
+                <p v-else class="text-red-700 absolute z-10 top-[-2rem]"></p>
+                <input
+                  id="search-dropdown"
+                  v-model="todo"
+                  class="block p-2 xl:p-4 pr-16 w-full z-20 text-[16px] xl:text-2xl text-white bg-bgSecondary drop-shadow-3xl rounded-r-xl border-none focus:ring-transparent"
+                  placeholder="Create a new todo..."
+                  required
+                  autocomplete="off"
+                />
+                <button
+                  type="submit"
+                  @click.prevent="addTodo"
+                  class="absolute text-center w-10 h-10 xl:w-auto xl:h-auto top-0 right-0 xl:p-2.5 text-2xl font-medium text-white bg-addBtn rounded-r-lg border hover:bg-addBtn border-none"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="h-6 w-6 m-0 mx-auto xl:w-11 xl:h-11"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
+        <div
+          class="container-todoItem text-white mt-10 max-h-[50%] drop-shadow-3xl"
+        >
+          <TodoItem
+            :todos="todos"
+            @deleteTodo="deleteTodo"
+            @finishTodo="finishTodo"
+          />
+        </div>
+        <p class="p-2.5 text-white mt-3 text-[16px] xl:text-3xl">
+          Finish Todo : {{ checkLength() }}
+        </p>
       </div>
-      <div
-        class="container-todoItem text-white mt-10 max-h-[50%] drop-shadow-3xl"
-        :class="checkHeight"
-      >
-        <TodoItem
-          :todos="todos"
-          @deleteTodo="deleteTodo"
-          @finishTodo="finishTodo"
-        />
-      </div>
-      <p class="p-2.5 text-white mt-3 text-[16px] xl:text-3xl">
-        Jumlah Todo : {{ todoLength }}
-      </p>
     </div>
   </div>
 </template>
@@ -80,28 +81,20 @@ export default {
     return {
       todos: [],
       todo: "",
-      check: "",
+      todoDone: [],
     };
   },
-  watch: {
-    todos() {
-      console.log("yyy");
-    },
-  },
+
+  watch: {},
 
   computed: {
-    todoLength() {
-      return this.todos.length;
-    },
-
-    checkHeight() {
-      let num = this.todoLength.toString();
-
-      if (num > 5) {
-        return "h-[36rem]";
-      }
-      return "h-auto";
-    },
+    // checkHeight() {
+    //   let num = this.todoLength.toString();
+    //   if (num > 5) {
+    //     return "h-[36rem]";
+    //   }
+    //   return "h-auto";
+    // },
   },
 
   mounted() {
@@ -123,6 +116,7 @@ export default {
       this.todos = this.todos.filter((item, index) => {
         if (index !== todoIndex) {
           return item;
+          // return item;
         }
       });
       this.saveTodo();
@@ -132,12 +126,8 @@ export default {
       this.todos.forEach((element, index) => {
         if (todoIndex == index) {
           element.done = !element.done;
-          console.log(element);
-          this.check = element.done;
-          console.log(this.check);
         }
       });
-
       this.saveTodo();
     },
 
@@ -146,7 +136,13 @@ export default {
       localStorage.setItem("todos", dataParse);
     },
 
-    // checked,
+    checkLength() {
+      let counter = 0;
+      for (let i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].done === true) counter++;
+      }
+      return counter;
+    },
   },
 };
 </script>
